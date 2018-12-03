860f79dd0531a643adb5547e16716a86f402c9f6
3Srk <kakizoe@3s-style.com>
2018-12-03 23:47:16 +0900

    2
    2

--- test/gate_test.rb
+++ test/gate_test.rb
@@ -21,18 +21,20 @@ require './lib/ticket'
 
 class GateTest < Minitest::Test
     # Minitestではsetupメソッドを定義すると、テストメソッドの実行前に毎回setupメソッドが呼び出される
-
+    def setup
+        @umeda = Gate.new(:umeda)
+        @juso = Gate.new(:juso)
+        @mikuni = Gate.new(:mikuni)
+    end
     # シナリオ1（1区間）
     # 150円の切符を購入する
     # 梅田で入場し、十三で出場する
     # 期待する結果：出場できる
     def test_umeda_to_juso
-        umeda = Gate.new(:umeda)
-        juso =  Gate.new(:juso)
         ticket = Ticket.new(150)
-        umeda.enter(ticket)
+        @umeda.enter(ticket)
         # assert(boolean, message=nil)で、booleanがtrueと評価されればテスト成功
-        assert juso.exit(ticket)
+        assert @juso.exit(ticket)
     end
 
     # シナリオ2（2区間・運賃不足）
@@ -40,11 +42,20 @@ class GateTest < Minitest::Test
     # 梅田で入場し、三国で出場する
     # 期待する結果：出場できない
     def test_umeda_to_mikuni_when_fare_is_not_enough
-        umeda = Gate.new(:umeda)
-        mikuni =  Gate.new(:mikuni)
-
         ticket = Ticket.new(150)
-        umeda.enter(ticket)
-        refute mikuni.exit(ticket)
+        @umeda.enter(ticket)
+        refute @mikuni.exit(ticket)
+    end
+
+    def test_umeda_to_mikuni_when_fare_is_enough
+        ticket = Ticket.new(190)
+        @umeda.enter(ticket)
+        assert @mikuni.exit(ticket)
+    end
+
+    def test_juso_to_mikuni
+        ticket = Ticket.new(150)
+        @juso.enter(ticket)
+        assert @mikuni.exit(ticket)
     end
 end
